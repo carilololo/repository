7484189d7a0fbcdfff11b6194458fe3d00512f1c
John Alexander Carrillo Rodriguex <37900934-johnalexandercr@users.noreply.replit.com>
2024-11-20 01:57:24 +0000

    Subo tercera app

--- docker/segunda-aplicacion/app.py
+++ docker/segunda-aplicacion/app.py
@@ -5,6 +5,5 @@ app = Flask(__name__)
 @app.route('/')
 def index():
   return 'Mi microservicio'
-
 if __name__ == '__main__':
-  app.run(debug=True, host='0.0.0.0', port=8080)
+  app.run(debug=True, host='0.0.0.0', port=5000)
